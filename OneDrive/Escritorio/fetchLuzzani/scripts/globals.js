f0c3ba9093ed64830028830f900dc40c6f17be21
Lucas Luzzani <lucasluzzani@gmail.com>
2023-03-24 11:29:46 -0300

    Aplicacion CRUD de productos en react-native terminada

--- OneDrive/Escritorio/fetchLuzzani/scripts/globals.js
+++
@@ -1,8 +0,0 @@
-// array libros
-
-const listaLibros = [];
-const carrito = [];
-
-
-const contentModal = document.getElementById("modalContent");
-const cantidadCarrito = document.getElementById("cont");
